fdd71b2e970b070ae3f0ae78bb17c7517b5d4d13
Xiao S Zheng <tuf48938@temple.edu>
2016-10-10 15:10:01 -0400

    Editted Source Code for Lab 3

--- XML/src/HttpURLConnectionExample.java
+++ XML/src/HttpURLConnectionExample.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
@@ -5,6 +6,7 @@ import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
+import java.util.Scanner;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.xml.parsers.DocumentBuilder;
@@ -14,96 +16,98 @@ import org.xml.sax.InputSource;
 
 public class HttpURLConnectionExample {
 
-	private final String USER_AGENT = "Mozilla/5.0";
+    private final String USER_AGENT = "Mozilla/5.0";
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		HttpURLConnectionExample http = new HttpURLConnectionExample();
+        HttpURLConnectionExample http = new HttpURLConnectionExample();
 
-		System.out.println("Testing 1 - Send Http GET request");
-		http.sendGet();
+        System.out.println("Testing 1 - Send Http GET request");
+        http.sendGet();
 
+    }
 
-	}
+    // HTTP GET request
+    private void sendGet() throws Exception {
 
-	// HTTP GET request
-	private void sendGet() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your TUID: ");
+        int tuid = sc.nextInt();
+        String url = "http://129.32.92.49:8080/xml_lab/getgrade?xml=<tuid>" + tuid + "</tuid>";
 
-		String url = "http://129.32.92.49:8080/xml_lab/getgrade?xml=<tuid>123456</tuid>";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // optional default is GET
+        con.setRequestMethod("GET");
 
-		// optional default is GET
-		con.setRequestMethod("GET");
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
 
-		//add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
 
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
 
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
+        //print result
+        System.out.println(response.toString());
+        String input = response.toString();
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(new InputSource(new StringReader(input)));
+        doc.getDocumentElement().normalize();
+        String grade = doc.getElementsByTagName("content").item(0).getTextContent();
+        System.out.println("Grade: " + grade);
+    }
 
-		//print result
-		System.out.println(response.toString());
-                String input = response.toString();
-                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-                Document doc = dBuilder.parse(new InputSource(new StringReader(input)));
-                doc.getDocumentElement().normalize();
-                String grade = doc.getElementsByTagName("content").item(0).getTextContent();
-                System.out.println("Grade: " + grade);
-	}
+    // HTTP POST request
+    private void sendPost() throws Exception {
 
-	// HTTP POST request
-	private void sendPost() throws Exception {
+        String url = "https://selfsolve.apple.com/wcResults.do";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
-		String url = "https://selfsolve.apple.com/wcResults.do";
-		URL obj = new URL(url);
-		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-		//add reuqest header
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
 
-		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
 
-		// Send post request
-		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(urlParameters);
-		wr.flush();
-		wr.close();
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + urlParameters);
-		System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
 
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
 
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
+        //print result
+        System.out.println(response.toString());
 
-		//print result
-		System.out.println(response.toString());
-
-	}
+    }
 
 }
